7a4e767c538887bbbcdcbcae5eb10d1410ad4a01
Mehmet Turunç <mehmetturuncx@gmail.com>
2026-08-03 18:07:21 +0300

    test: Yeni testler eklendi

--- tests/kullanici.test.ts
+++ tests/kullanici.test.ts
@@ -1,29 +1,64 @@
 import request from 'supertest';
 import { app, prisma } from '../src/index.js'; 
-
-
+import { redis } from '../src/redis.js'; 
+import { email } from 'zod';
 
 describe('Kullanıcı API Testleri', () => {
 
+    let createdEmail: string;
+
     afterAll(async () => {
         await prisma.$disconnect();
-    })
-
-    it('GET /api/kullanici - Tüm kullanıcıları listeleyebilmeli', async () => {
-        const response = await request(app).get('/api/kullanici');
-
-        expect(response.status).toBe(200);
-        expect(Array.isArray(response.body)).toBe(true);
+        await redis.quit(); 
     });
 
-    it('POST /api/auth/register', async () => {
+    it('GET /api/kullanici - Kullanıcıları sayfalamalı getirebilmeli', async () => {
+        const response = await request(app).get('/api/kullanici?sayfa=1&limit=5');
+
+        expect(response.status).toBe(200);
+        
+        expect(response.body).toHaveProperty('data');
+        expect(Array.isArray(response.body.data)).toBe(true);
+        
+        expect(response.body).toHaveProperty('sayfa', 1);
+        expect(response.body).toHaveProperty('kaynak');
+    });
+
+    it('POST /api/auth/register - Geçersiz emaili reddetmeli', async () => {
         const response = await request(app)
                 .post('/api/auth/register')
                 .send({
-                    email: "invaild-email"
+                    email: "invalid-email"
                 });
 
         expect(response.status).toBe(400);
         expect(response.body).toHaveProperty('hatalar');
+    });
+
+    it('POST /api/auth/register', async () => {
+        const dynamicEmail = `testuser_${Date.now()}@test.com`;
+        const response = await request(app)
+                .post('/api/auth/register')
+                .send({
+                    email: dynamicEmail,
+                    isim: "testuser",
+                    sifre: "123456789"
+                });
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('kullanici');
+        createdEmail = dynamicEmail;
+    });
+
+    it('POST /api/auth/login', async () => {
+        const response = await request(app)
+                .post('/api/auth/login')
+                .send({
+                    email: createdEmail,
+                    sifre: "123456789"
+                });
+        
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('token');
     })
 });
